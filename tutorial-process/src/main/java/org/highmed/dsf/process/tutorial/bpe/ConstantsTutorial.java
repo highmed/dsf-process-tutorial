ffd40fb2f0cd5f629ec737184c911db9a5ca65de
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2022-08-15 15:18:50 +0200

    adds constants class

---
+++ tutorial-process/src/main/java/org/highmed/dsf/process/tutorial/bpe/ConstantsTutorial.java
@@ -0,0 +1,17 @@
+package org.highmed.dsf.process.tutorial.bpe;
+
+import static org.highmed.dsf.bpe.ConstantsBase.PROCESS_HIGHMED_URI_BASE;
+import static org.highmed.dsf.process.tutorial.bpe.TutorialProcessPluginDefinition.VERSION;
+
+public interface ConstantsTutorial
+{
+	String PROCESS_NAME_HELLO_WORLD = "helloWorld";
+	String PROCESS_NAME_FULL_HELLO_WORLD = "highmedorg_" + PROCESS_NAME_HELLO_WORLD;
+
+	String PROFILE_TUTORIAL_TASK_HELLO_WORLD = "http://highmed.org/fhir/StructureDefinition/task-hello-world";
+	String PROFILE_TUTORIAL_TASK_HELLO_WORLD_AND_LATEST_VERSION = PROFILE_TUTORIAL_TASK_HELLO_WORLD + "|" + VERSION;
+	String PROFILE_TUTORIAL_TASK_HELLO_WORLD_PROCESS_URI = PROCESS_HIGHMED_URI_BASE + PROCESS_NAME_HELLO_WORLD + "/";
+	String PROFILE_TUTORIAL_TASK_HELLO_WORLD_PROCESS_URI_AND_LATEST_VERSION = PROFILE_TUTORIAL_TASK_HELLO_WORLD_PROCESS_URI
+			+ VERSION;
+	String PROFILE_TUTORIAL_TASK_HELLO_WORLD_MESSAGE_NAME = "helloWorldMessage";
+}
